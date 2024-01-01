6b135c4d54e63f8dee3efcfe13574513fe01894c
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-01 21:17:51 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,11 +14,11 @@ class MyDocument extends Document {
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta name="twitter:site" content="@Rxz" />
+          <meta name="twitter:creator" content="@Rxz" />
+          <meta property="og:url" content="https://rxz-bot.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="Rxz" />
           <link
             rel="icon"
             href="/img/logo2.png"
