433f2c8033c6772805020bbce39495bda7a1aa16
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-01 21:23:28 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -44,7 +44,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Add Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands",
     },
        {
       link: true,
