4256c9cf8634f81be6f4df994ba5af177a41d5fe
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 22:17:03 +0300

    Update thanks.jsx

--- pages/thanks.jsx
+++ pages/thanks.jsx
@@ -3,13 +3,11 @@ export default function Thanks() {
     <div>
       <img width="500" src="/img/bck.png" />
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Rxz
+          Thanks!
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Rxz? Make server management and moderation easy. Improve your server security, run giveaways, and more! 
+       Thanks for adding Rxz!
 	  </p>
-     <h2 className="mt-4 text-white font-bold text-3xl">Thanks</h2>
-      <p className="text-sm text-white/30 mb-1">Thanks for adding Rxz!</p>
     </div>
   );
 }
