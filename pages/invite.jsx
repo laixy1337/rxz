4cfe0f4e5c1b723c10f574bd5c8907b1ea2b55f8
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 22:49:28 +0300

    Update and rename invite.jsx to _middleware.jsx

--- pages/invite.jsx
+++
@@ -1,6 +0,0 @@
-export default function Invite() {
-    <div>
-    <Link href="https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands">
-    </Link>
-  </div>
-}
