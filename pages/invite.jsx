fd8d9b5cb01d98c62508f164548edcd5332f6708
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 22:27:48 +0300

    Create invite.jsx

---
+++ pages/invite.jsx
@@ -0,0 +1,11 @@
+export default function Thanks() {
+  return (
+ <html>
+   <head>
+      <meta http-equiv = "refresh" content = 0.5; url = https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands" />
+   </head>
+   <body>
+   </body>
+</html>
+  );
+}
