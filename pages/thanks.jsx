65293f6661b5729687d5ad79ae7011db98ab6b63
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 22:06:40 +0300

    Update thanks.jsx

--- pages/thanks.jsx
+++ pages/thanks.jsx
@@ -1,38 +1,8 @@
-import Link from "next/link";
-import Image from "next/image";
-
-export default function Index() {
-	
+export default function Thanks() {
   return (
-    <>
-      <div className="py-20 mb-30 max-w-3xl mx-auto">
-        <div className="flex w-full justify-center">
-      <img width="500" src="/img/bck.png" />
-      </div>
-        <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Rxz
-        </p>
-        <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Rxz? Make server management and moderation easy. Improve your server security, run giveaways, and more! 
-	  </p>
-        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
-  
-            <Link href={"https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands"}>
-              <a
-                className={
-                  "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
-                }
-              >
-          Invite
-              </a>
-            </Link>
-  <div className="py-10"></div>
-  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
-  <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote</a>
-  </Link>
-		
-
-      </>
-  )
+    <div>
+     <h2 className="mt-4 text-white font-bold text-3xl">Thanks for adding Rxz!</h2>
+</div>
+    </div>
+  );
 }
