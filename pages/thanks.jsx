6130ed9abba124c1f62ed66724d2bea086b45e8f
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 21:55:37 +0300

    Update thanks.jsx

--- pages/thanks.jsx
+++ pages/thanks.jsx
@@ -1,25 +1,8 @@
-
-export default function Partners() {
-
-    return (
-        <>
-            <div className="w-full my-10">
-            <div className="flex w-full justify-center">
-      <img width="400" src="/img/bck.png" />
-      </div>
-                <h1 className="py-7 animateHeader text-4xl font-extrabold text-center text-white">
-                    <i className="fal fa-stars text-amber-400 mr-2" />
-                    Thanks!
-                </h1>
-            </div>
-
-            <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
-
-            <div className="flex flex-col justify-center text-white rounded">
-    
-
-                </div>
-                <div className="py-10"></div>
-        </>
-    );
-};
+export default function Thanks() {
+  return (
+    <div>
+     <h2 className="mt-4 text-white font-bold text-3xl">Thanks for adding <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">Rxz</a></h2>
+</div>
+    </div>
+  );
+}
