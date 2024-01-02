6873a7711c5c76da21a06202ae0e1e9c7c8a5e34
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 21:58:15 +0300

    Update thanks.jsx

--- pages/thanks.jsx
+++ pages/thanks.jsx
@@ -1,8 +1,24 @@
-export default function Thanks() {
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Index() {
+	
   return (
-    <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Thanks for adding <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">Rxz</a></h2>
-</div>
-    </div>
-  );
+    <>
+      <div className="py-20 mb-30 max-w-3xl mx-auto">
+        <div className="flex w-full justify-center">
+      <img width="500" src="/img/bck.png" />
+      </div>
+        <p className="animateHeader text-4xl font-extrabold text-center text-white">
+          Rxz
+        </p>
+        <p className="animateHeader text-white text-opacity-50 text-center mt-5">
+       How about moderating your own server with Rxz? Make server management and moderation easy. Improve your server security, run giveaways, and more! 
+	  </p>
+        <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
+
+        
+
+      </>
+  )
 }
