f9445de937604a77093ef75c7ee6ac6ff29393f9
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 22:37:14 +0300

    Update invite.jsx

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -1,4 +1,4 @@
 import redirect from 'next/navigation';
-export default async function Home({ params }) {
+export default function Invite() {
     redirect('https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands');
 }
