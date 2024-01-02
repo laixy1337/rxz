eee06be2f12618edeb8341736f1612b2105ab979
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-02 22:09:32 +0300

    Update thanks.jsx

--- pages/thanks.jsx
+++ pages/thanks.jsx
@@ -1,7 +1,8 @@
 export default function Thanks() {
   return (
     <div>
-     <h2 className="mt-4 text-white font-bold text-3xl">Thanks for adding Rxz!</h2>
+     <h2 className="mt-4 text-white font-bold text-3xl">Thanks</h2>
+      <p className="text-sm text-white/30 mb-1">Thanks for adding Rxz!</p>
 </div>
     </div>
   );
