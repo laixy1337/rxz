0ee853a541e527991be464b175843e7fc4b20f70
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-04 22:22:35 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,7 +2,7 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 01/01/2024 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 01/01/2024</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
 <p>⊳ Every discord server can use the Rxz for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
