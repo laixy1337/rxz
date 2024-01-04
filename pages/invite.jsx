fdab9e145c966e12c46f5a2386901106a8dda468
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-04 21:20:00 +0300

    Update invite.jsx

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -1,5 +1,3 @@
-// pages/redirect.jsx
-
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
@@ -7,7 +5,6 @@ const RedirectPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    // Replace '/target-page' with the actual path you want to redirect to
     router.push('https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands&redirect_uri=https%3A%2F%2Frxz-bot.vercel.app%2Fthanks&response_type=code');
   }, []);
 
