ae908d1cbe87df0aaa5c275f486962a17e3169a3
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-04 21:19:07 +0300

    Update invite.jsx

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -8,7 +8,7 @@ const RedirectPage = () => {
 
   useEffect(() => {
     // Replace '/target-page' with the actual path you want to redirect to
-    router.push('https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands');
+    router.push('https://discord.com/api/oauth2/authorize?client_id=1191154780287008858&permissions=8&scope=bot+applications.commands&redirect_uri=https%3A%2F%2Frxz-bot.vercel.app%2Fthanks&response_type=code');
   }, []);
 
   // Empty component or custom loading message can be added here
