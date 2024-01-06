3bd8bc1fe4d01436a39e03f64d8fa24fab6115b1
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-06 22:38:25 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="How about increasing the security level of your server to the highest level?"
           />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@Rxz" />
