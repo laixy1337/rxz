ad6cbe9f52d58076e1b144a363d6e6f355fb0f53
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-09 21:59:30 +0300

    Update thanks.jsx

--- pages/thanks.jsx
+++ pages/thanks.jsx
@@ -4,14 +4,15 @@ export default function Thanks() {
 	<div className="py-20 mb-30 max-w-3xl mx-auto">
 	<div className="flex w-full justify-center">
       <img width="500" src="/img/bck.png" />
+	</div>
+	</div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           Thanks!
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
        Thanks for adding Rxz!
 	  </p>
-	</div>
-	</div>
+
     </div>
   );
 }
