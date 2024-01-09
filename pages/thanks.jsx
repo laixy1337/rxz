5002fcd72085528b9eb6079bfe2c2ff20de95a54
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-09 21:57:32 +0300

    Update thanks.jsx

--- pages/thanks.jsx
+++ pages/thanks.jsx
@@ -1,8 +1,9 @@
 export default function Thanks() {
   return (
     <div>
-      <img width="500" src="/img/bck.png" />
+	<div className="py-20 mb-30 max-w-3xl mx-auto">
 	<div className="flex w-full justify-center">
+      <img width="500" src="/img/bck.png" />
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           Thanks!
         </p>
