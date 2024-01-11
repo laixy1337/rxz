3f4803499fa2a549424db547829587fe9837d426
laixy <125830449+laixy1337@users.noreply.github.com>
2024-01-11 19:57:35 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ZcDprCrFTn",
+      href: "https://discord.gg/zmCq784PRC",
     },
     {
       link: true,
